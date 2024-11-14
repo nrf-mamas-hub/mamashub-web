4c2f784c4b6707238b9024d6131630def70c3a3c
1wes <okemwawes@gmail.com>
2024-11-14 23:05:43 +0300

    feat(api): create function for submitting immunization details to immunization endpoint

--- ui/src/lib/api.js
+++ ui/src/lib/api.js
@@ -89,3 +89,36 @@ export const createAppointment = async (appointmentDetails) => {
         return null
     }
 }
+
+export const createImmunization = async (immunizationDetails) => {
+    
+    try {
+
+        let res = await (await FhirApi({
+            url: `/crud/immunization`,
+            method: "POST",
+            data: JSON.stringify({
+                patientId: immunizationDetails.patientId,
+                encounterId: immunizationDetails.encounterId,
+                practitionerId: immunizationDetails.practitionerId,
+                manufacturerId:immunizationDetails.manufacturerId,
+                vaccine: {
+                    name: immunizationDetails.name,
+                    immunizationDate: immunizationDetails.immunizationDate,
+                    lotNumber: immunizationDetails.lotNumber,
+                    expiryDate: immunizationDetails.expiryDate,
+                    site: immunizationDetails.site,
+                    route: immunizationDetails.route,
+                    dosage: immunizationDetails.dosage,
+                    additionalComments: immunizationDetails.additionalComments,
+                    unit: immunizationDetails.unit
+                }
+            })
+        })).data;
+
+        return res;
+        
+    } catch (error) {
+        return null;
+    }
+}
