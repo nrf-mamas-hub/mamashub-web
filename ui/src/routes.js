b1b98a513360cbdcbcb527454bd10975652578bf
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-04 16:08:10 +0300

    feat(MicronutrientPowders): add routes and form for micronutrient powders

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -27,6 +27,7 @@ import Facilites from './pages/Facilities';
 import MOH711 from './pages/MOH711';
 import Index from './pages/Index';
 import NurseDashboard from './pages/NurseDashboard';
+import MicronutrientPowders from './pages/MicronutrientPowders';
 
 const appRoutes = [
   {
@@ -148,6 +149,10 @@ const appRoutes = [
   {
     path: '/nurse-dashboard',
     element: NurseDashboard,
+  },
+  {
+    path: '/micronutrient-powders',
+    element: MicronutrientPowders,
   }
 ];
 
