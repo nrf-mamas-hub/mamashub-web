31ffea2c5c52c761b4405e9c22a34b2f38dc6dc5
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-12-10 20:15:15 +0300

    feat(RecorfOfBabyTeethDevelopment):display BabyTeethRecordForm

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -27,6 +27,7 @@ import Facilites from './pages/Facilities';
 import MOH711 from './pages/MOH711';
 import Index from './pages/Index';
 import NurseDashboard from './pages/NurseDashboard';
+import RecordOfBabyTeethDevelopment from './pages/RecordOfBabyTeethDevelopment';
 
 const appRoutes = [
   {
@@ -148,6 +149,10 @@ const appRoutes = [
   {
     path: '/nurse-dashboard',
     element: NurseDashboard,
+  },
+  {
+    path: '/baby-teeth-development',
+    element: RecordOfBabyTeethDevelopment,
   }
 ];
 
