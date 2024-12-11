cef749aa4c1d21a26db850b8c9c0fb60995ae236
1wes <okemwawes@gmail.com>
2024-12-11 15:03:37 +0300

    feat(babyTeethDevelopment): add error messages for form validations

--- ui/src/lib/forms/babyTeethDevelopment.js
+++ ui/src/lib/forms/babyTeethDevelopment.js
@@ -3,14 +3,14 @@ import * as yup from 'yup';
 const BabTeethDevelopment = {
   "Baby's Teeth Development": [ 
     {
-      name: 'teethObservation',
+      name: 'toothObservation',
       label: 'Observation',
       type: 'select',
       required: true,
       width: {
         xs: 12,
         sm: 12,
-        md: 12,
+        md: 8,
         lg: 6,
       },
       options: [
@@ -23,261 +23,34 @@ const BabTeethDevelopment = {
         { value: 'Lower second molar', label: 'Lower Second Molar (24-30 months)' },
         { value: 'Upper second molar', label: 'Upper second Molar (24-30 months)' },
       ],    
-      validate: yup.string().required('Teeth observation is required'),      
+      validate: yup.string().required('Tooth observation is required'),      
     },
     {
       name: 'ageOfBabyWhenToothSeen',
       label: 'Age of baby when tooth seen',
       type: 'text',
-     validate: yup.number(), 
+     validate: yup.number().required("Age is required"), 
       width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
+        xs: 12,
+        sm: 12,
+        md: 8,
+        lg: 6,
       },
-      relevant: (formValues) =>formValues.BabyTeethDevelopment === 'LowerIncisor' &&
-      !(formValues.lowerIncisorFourToTenMonthsDateSeen && formValues.lowerIncisorFourToTenMonthsAgeOfBabyWhenToothSeen),
+      relevant: (formValues) => formValues.toothObservation,
     },
     {
-      name: 'lowerIncisorFourToTenDateSeen',
-      label: 'If present, date seen',
+      name: 'dateToothSeen',
+      label: 'Date seen',
       type: 'date',
-      validate: yup.date(),
+      validate: yup.date().required("Date is required"),
       width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
+        xs: 12,
+        sm: 12,
+        md: 8,
+        lg: 6,
       },
-      relevant: (formValues) => 
-        formValues.BabyTeethDevelopment === 'LowerIncisor' &&
-        !(formValues.lowerIncisorFourToTenMonthsDateSeen && formValues.lowerIncisorFourToTenMonthsAgeOfBabyWhenToothSeen),
-    
+      relevant: (formValues) => formValues.toothObservation,
     }, 
-    //upper incisor 
-    {
-      name: 'upperIncisorSixToTwelveMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-       
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: (formValues) => formValues.BabyTeethDevelopment === 'upperIncisor' && !formValues.upperIncisorSixToTwelveMonthsDateSeen,
-      
-    },
-    
-    {
-      name: 'upperIncisorSixToTwelveMonthsDateSeen',
-      label: 'If Present, date seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'upperIncisor',
-        // formValues.upperIncisor === 'Yes',
-    
-},
-    //lower canine
-    {
-      name: 'lowerCanineTwelveToTwentyThreeMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: formValues =>formValues.BabyTeethDevelopment === 'lowerCanine',
-      
-    },
-
-    {
-      name: 'lowerCanineTwelveToTwentyThreeMonthsDateSeen',
-      label: 'If yes, date seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'lowerCanine',
-    },
-    //upper canine
-    {
-      name: 'upperCanineTwelveToTwentyThreeMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-        
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-        },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'upperCanine',
-      
-    },
-
-    {
-      name: 'upperCanineTwelveToTwentyThreeMonthsDateSeen',
-      label: 'If yes, date Seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'upperCanine',
-    },
-  
-    //lower first molar
-    {
-      name: 'lowerFirstMolarTwelveToEighteenMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-        
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'lowerFirstMolar',
-      
-    },
-
-    {
-      name: 'lowerFirstMolarTwelveToEighteenMonthsDateSeen',
-      label: 'If yes, date seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'lowerFirstMolar',
-    },
-    //upper first molar
-    {
-      name: 'upperFirstMolarTwelveToEighteenMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-      
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: formValues =>  formValues.BabyTeethDevelopment === 'upperFirstMolar',
-      
-    },
-
-    {
-      name: 'upperFirstMolarTwelveToEighteenMonthsDateSeen',
-      label: 'If yes, date seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues =>formValues.BabyTeethDevelopment === 'upperFirstMolar',
-    },
-    //lower second molar
-    {
-      name: 'lowerSecondMolarTwentyFourToThirtyMonthsAgeOfBabyWhenToothSeen',
-      label: 'Age of baby when tooth seen (in months)',
-      type: 'text',
-      validate: yup
-        .number(),
-       
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'lowerSecondMolar',
-      
-    },
-
-    {
-      name: 'lowerSecondMolarTwentyFourToThirtyMonthsDateSeen',
-      label: 'If yes, date seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'lowerSecondMolar',
-    },
-    //upper second molar
-    {
-      name: 'upperSecondMolarTwentyFourToThirtyMonthsAgeOfBabyWhenToothSeen',
-      label: "baby's Age when tooth seen (in months)",
-      type: 'text',
-      validate: yup
-        .number(),
-       
-      width: {
-        xs: 5,
-        sm: 5,
-        md: 5,
-        lg: 4,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'upperSecondMolar',
-      
-    },
-
-  
-    {
-      name: 'upperSecondMolarTwentyFourToThirtyMonthsDateSeen',
-      label: 'If yes, date Seen',
-      type: 'date',
-      validate: yup.date(),
-      width: {
-        xs: 4,
-        sm: 4,
-        md: 4,
-        lg: 3,
-      },
-      relevant: formValues => formValues.BabyTeethDevelopment === 'upperSecondMolar',
-    },
-    
-    
-    
   ],
 };
 export default BabTeethDevelopment;
