330f2a2e3fad82514d817a1b1692a9089a9f4a84
Amirul Faqih <fakiiahmad001@gmail.com>
2024-12-18 00:05:48 +0300

    fix(MicronutrientPowders): optimize ordinal suffix function and update encounter limit

--- ui/src/pages/MicronutrientPowders.jsx
+++ ui/src/pages/MicronutrientPowders.jsx
@@ -198,17 +198,15 @@ export default function MotherPostnatalCare({ userData }) {
       const ordinalSuffix = (i) => {
         let j = i % 10,
           k = i % 100;
-        if (j === 1 && k !== 11) {
-          return i + "st";
-        }
-        if (j === 2 && k !== 12) {
-          return i + "nd";
-        }
-        if (j === 3 && k !== 13) {
-          return i + "rd";
-        }
+        if (j === 1 && k !== 11) return i + "st";
+        if (j === 2 && k !== 12) return i + "nd";
+        if (j === 3 && k !== 13) return i + "rd";
         return i + "th";
       };
+      
+      for (let i = 1; i <= 17; i++) {
+        console.log(ordinalSuffix(i));
+      }      
     
       return (
         <>
@@ -277,7 +275,7 @@ export default function MotherPostnatalCare({ userData }) {
                                 </Grid>
                               );
                             })}
-                          {micronutrientPowdersEncounters.length < 4 && (
+                          {micronutrientPowdersEncounters.length < 17 && (
                             <Grid
                               item
                               xs={12}
